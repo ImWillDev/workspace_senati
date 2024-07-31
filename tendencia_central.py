8010d1702c5b55b105dd4ac178bd60c08e01f23a
ImWillDev <1529304@senati.pe>
2024-07-31 11:48:29 -0500

    se agrega un ejercicio

---
+++ tendencia_central.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+muestra = np.array([83.5, 81, 69.3, 74.5, 80.2, 77.3, 74.9, 79.6, 78.8, 78.1, 70.8, 65.1, 73.8, 69.2, 66.2, 73.3, 76, 68.3])
+
+
+print(muestra.dtype)
